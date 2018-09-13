c1fee39302e4ea9c778e304f42f471a22db7c9c3
Sarah Waldie <sarah@sassafrasbiz.com>
2018-09-13 16:02:06 -0400

    Books move shelves and default value in dropdown updates.

--- src/Book.js
+++ src/Book.js
@@ -4,6 +4,32 @@ class Book extends Component {
 
 
 
+    render() {
+        const { book, currentShelf } = this.props
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                    <div className="book-shelf-changer">
+                        <select
+                            onChange={(event) => this.props.handleChange(this.props.book, event.target.value)}
+                            value={currentShelf}
+                        >
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
+            </div>
+        )
+    }
+
+
 
 }
 
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,10 +1,83 @@
 import React, { Component } from 'react'
+import Book from './Book'
 
 class MainPage extends Component {
 
+    render() {
 
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Currently Reading</h2>
+                            <ol className="books-grid">
+                                {
+                                    this.props.books
+                                        .filter(book => book.shelf === 'currentlyReading')
+                                        .map(book => (
+                                            <li>
+                                                <Book
+                                                    book={book}
+                                                    handleChange={this.props.handleChange}
+                                                    currentShelf="currentlyReading"
+                                                />
+                                            </li>
+                                        )
+                                        )
+                                }
+                            </ol>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Want to Read</h2>
+                            <ol className="books-grid">
+                                {
+                                    this.props.books
+                                        .filter(book => book.shelf === 'wantToRead')
+                                        .map(book => (
+                                            <li>
+                                                <Book
+                                                    book={book}
+                                                    handleChange={this.props.handleChange}
+                                                    currentShelf="wantToRead"
+                                                />
+                                            </li>
+                                        )
+                                        )
+                                }
+                            </ol>
+                        </div>
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">Read</h2>
+                            <ol className="books-grid">
+                                {
+                                    this.props.books
+                                        .filter(book => book.shelf === 'read')
+                                        .map(book => (
+                                            <li>
+                                                <Book
+                                                    book={book}
+                                                    handleChange={this.props.handleChange}
+                                                    currentShelf="read"
+                                                />
+                                            </li>
+                                        )
+                                        )
+                                }
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div className="open-search">
+                    <a onClick={() => this.props.showSearchPage}>Add a book</a>
+                </div>
+            </div>
+        )
 
-
+    }
 }
 
 export default MainPage
