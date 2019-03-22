a512e16350b8a86f4a8a0fc690b17a2ce953e9f6
Sarah Waldie <sarah@sassafrasbiz.com>
2019-03-22 16:16:32 -0400

    converted main page to functional component b/c it is stateless

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,51 +1,40 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Shelf from './Shelf'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Shelf from "./Shelf";
 
-class MainPage extends Component {
-
-    render() {
-
-        console.log(this.props.books)
-
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <Shelf
-                            books={this.props.books}
-                            handleChange={this.props.handleChange}
-                            currentShelf="currentlyReading"
-                            isMain={this.props.isMain}
-                        />
-                        <Shelf
-                            books={this.props.books}
-                            handleChange={this.props.handleChange}
-                            currentShelf="wantToRead"
-                            isMain={this.props.isMain}
-                        />
-                        <Shelf
-                            books={this.props.books}
-                            handleChange={this.props.handleChange}
-                            currentShelf="read"
-                            isMain={this.props.isMain}
-                        />
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link
-                        to='/search'
-                    >Add a book</Link>
-                </div>
-            </div>
-        )
-
-    }
+function MainPage({ books, isMain, handleChange }) {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <Shelf
+            books={books}
+            handleChange={handleChange}
+            currentShelf="currentlyReading"
+            isMain={isMain}
+          />
+          <Shelf
+            books={books}
+            handleChange={handleChange}
+            currentShelf="wantToRead"
+            isMain={isMain}
+          />
+          <Shelf
+            books={books}
+            handleChange={handleChange}
+            currentShelf="read"
+            isMain={isMain}
+          />
+        </div>
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  );
 }
 
-export default MainPage
-
-
+export default MainPage;
