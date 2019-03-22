5c4acccddd5ce527db5e98696c786cf855b7c466
Sarah Waldie <sarah@sassafrasbiz.com>
2019-03-22 16:12:22 -0400

    converted Book component to functional b/c it is stateless

--- src/Book.js
+++ src/Book.js
@@ -2,55 +2,47 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import ShelfStatus from "./ShelfStatus";
 
-class Book extends Component {
-  render() {
-    const { book, currentShelf } = this.props;
+function Book({ book, currentShelf, isSearch, handleChange }) {
+  let thumbImage = book.imageLinks ? book.imageLinks.thumbnail : "";
 
-    let thumbImage = this.props.book.imageLinks
-      ? this.props.book.imageLinks.thumbnail
-      : "";
+  let path = `books/${book.id}`;
 
-    let path = `books/${book.id}`;
-
-    return (
-      <div className="book">
-        {this.props.isSearch && (
-          <div className="book-title">
-            <ShelfStatus shelf={currentShelf} />
-          </div>
-        )}
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{
-              width: 128,
-              height: 193,
-              backgroundImage: `url("${thumbImage}")`
-            }}
-          />
-          <div className="book-shelf-changer">
-            <select
-              onChange={event =>
-                this.props.handleChange(this.props.book, event.target.value)
-              }
-              value={currentShelf}
-            >
-              <option value="move" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+  return (
+    <div className="book">
+      {isSearch && (
+        <div className="book-title">
+          <ShelfStatus shelf={currentShelf} />
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
-        <Link to={path}>Info</Link>
+      )}
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{
+            width: 128,
+            height: 193,
+            backgroundImage: `url("${thumbImage}")`
+          }}
+        />
+        <div className="book-shelf-changer">
+          <select
+            onChange={event => handleChange(book, event.target.value)}
+            value={currentShelf}
+          >
+            <option value="move" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+        </div>
       </div>
-    );
-  }
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.authors}</div>
+      <Link to={path}>Info</Link>
+    </div>
+  );
 }
 
 export default Book;
