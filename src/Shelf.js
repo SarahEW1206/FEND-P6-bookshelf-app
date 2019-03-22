a2145e167722f2b85528cb339e9fa26af70fbb2e
Sarah Waldie <sarah@sassafrasbiz.com>
2019-03-22 16:18:33 -0400

    converted Shelf component  to functional component b/c it is stateless

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,53 +1,35 @@
-import React, { Component } from 'react'
-import Book from './Book'
+import React, { Component } from "react";
+import Book from "./Book";
 
+function Shelf({ books, currentShelf, handleChange, isMain }) {
+  return (
+    <div className="bookshelf">
+      {currentShelf === "currentlyReading" && (
+        <h2 className="bookshelf-title">Currently Reading</h2>
+      )}
 
-class Shelf extends Component {
+      {currentShelf === "wantToRead" && (
+        <h2 className="bookshelf-title">Want to Read</h2>
+      )}
 
+      {currentShelf === "read" && <h2 className="bookshelf-title">Read</h2>}
 
-
-    render() {
-        const { books, currentShelf } = this.props
-
-        console.log(books)
-        return (
-            <div className="bookshelf">
-
-                {currentShelf === 'currentlyReading' &&
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                }
-
-                {currentShelf === 'wantToRead' &&
-                    <h2 className="bookshelf-title">Want to Read</h2>
-                }
-
-                {currentShelf === 'read' &&
-                    <h2 className="bookshelf-title">Read</h2>
-                }
-
-                <ol className="books-grid">
-                    {
-                        books
-                            .filter(book => book.shelf === currentShelf)
-                            .map(book => (
-                                <li key={book.id}>
-                                    <Book
-                                        book={book}
-                                        handleChange={this.props.handleChange}
-                                        currentShelf={this.props.currentShelf}
-                                        isMain={this.props.isMain}
-                                    />
-                                </li>
-                            )
-                            )
-                    }
-                </ol>
-            </div>
-        )
-    }
-
-
-
+      <ol className="books-grid">
+        {books
+          .filter(book => book.shelf === currentShelf)
+          .map(book => (
+            <li key={book.id}>
+              <Book
+                book={book}
+                handleChange={handleChange}
+                currentShelf={currentShelf}
+                isMain={isMain}
+              />
+            </li>
+          ))}
+      </ol>
+    </div>
+  );
 }
 
-export default Shelf
+export default Shelf;
